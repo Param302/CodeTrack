806ee5e31ee33c760469d1a49a7319aac1b90128
Parampreet Singh <rubercuber.30@gmail.com>
2024-12-03 19:44:30 +0530

    fixed pagepros error

--- codetrack/src/app/embed/[username]/page.tsx
+++ codetrack/src/app/embed/[username]/page.tsx
@@ -1,32 +1,25 @@
-import EmbedHeatmap from './EmbedHeatmap'
+import EmbedHeatmap from './EmbedHeatmap';
 
-type PageProps = {
-    params: { username: string },
-    searchParams?: { [key: string]: string | string[] | undefined }
+type Props = {
+  params: { username: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default async function Page({
-    params: rawParams,
-    searchParams: rawSearchParams
-}: PageProps) {
-    const params = await Promise.resolve(rawParams);
-    const searchParams = await Promise.resolve(rawSearchParams);
-    
-    return (
-      <>
-        <EmbedHeatmap
-          username={params.username}
-          gap={searchParams?.gap ? Number(searchParams.gap) : undefined}
-          borderRadius={searchParams?.borderRadius ? Number(searchParams.borderRadius) : undefined}
-          darkMode={searchParams?.darkMode === 'true'}
-          theme={searchParams?.theme?.toString()}
-          reverse={searchParams?.reverse === 'true'}
-          showTotalContributions={searchParams?.showTotalContributions === 'true'}
-          showProfileData={searchParams?.showProfileData === 'true'}
-          showTooltip={searchParams?.showTooltip === 'true'}
-          showWeekdays={searchParams?.showWeekdays === 'true'}
-          showMonths={searchParams?.showMonths === 'true'}
-          shareableSnapshot={searchParams?.shareableSnapshot === 'true'} />
-      </>
-    );
+export default function Page({ params, searchParams }: Props) {
+  return (
+    <EmbedHeatmap
+      username={params.username}
+      gap={searchParams?.gap ? Number(searchParams.gap) : undefined}
+      borderRadius={searchParams?.borderRadius ? Number(searchParams.borderRadius) : undefined}
+      darkMode={searchParams?.darkMode === 'true'}
+      theme={searchParams?.theme?.toString()}
+      reverse={searchParams?.reverse === 'true'}
+      showTotalContributions={searchParams?.showTotalContributions === 'true'}
+      showProfileData={searchParams?.showProfileData === 'true'}
+      showTooltip={searchParams?.showTooltip === 'true'}
+      showWeekdays={searchParams?.showWeekdays === 'true'}
+      showMonths={searchParams?.showMonths === 'true'}
+      shareableSnapshot={searchParams?.shareableSnapshot === 'true'}
+    />
+  );
 } 
